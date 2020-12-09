c6ed56e237ff463a9b2342aa9f3b836771339476
“Andreyka <dream.of.yaz@gmail.com>
2020-12-09 15:03:58 +0600

    implement

--- src/main/java/algorythms/BitonicSort.java
+++ src/main/java/algorythms/BitonicSort.java
@@ -8,8 +8,8 @@ public class BitonicSort implements Algorythm {
 
     private void compAndSwap(ArrayList<Integer> a, int i, int j, int dir) {
         if ((a.get(i) > a.get(j) && dir == 1) ||
-                (a.get(i) < a.get(j) && dir == 0)) {
-            int temp = a.get(i);
+                (a.get(i) < a.get(j) && dir == 0)) {//1
+            int temp = a.get(i);//1
             a.set(i, a.get(j));
             a.set(j, temp);
         }
@@ -17,35 +17,30 @@ public class BitonicSort implements Algorythm {
 
     private void bitonicMerge(ArrayList<Integer> a, int low, int cnt, int dir) {
         if (cnt > 1) {
-            int k = cnt / 2;
-            for (int i = low; i < low + k; i++)
-                compAndSwap(a, i, i + k, dir);
-            bitonicMerge(a, low, k, dir);
-            bitonicMerge(a, low + k, k, dir);
+            int k = cnt / 2;//1
+            for (int i = low; i < low + k; i++)//n
+                compAndSwap(a, i, i + k, dir);//log(n)
+            //log(n)*n
+            bitonicMerge(a, low, k, dir);//log(n)+1+log(n)*n
+            bitonicMerge(a, low + k, k, dir);//log(n)+1+log(n)*n
         }
     }
 
     private void bitonicSort(ArrayList<Integer> a, int low, int cnt, int dir) {
-        if (cnt > 1) {
-            int k = cnt / 2;
+        if (cnt > 1) {//1
+            int k = cnt / 2;//1
 
-            bitonicSort(a, low, k, 1);
+            bitonicSort(a, low, k, 1);//log(n)
 
-            bitonicSort(a, low + k, k, 0);
+            bitonicSort(a, low + k, k, 0);//log(n)
 
-            bitonicMerge(a, low, cnt, dir);
+            bitonicMerge(a, low, cnt, dir);//1+n+log(n)*n+log(n)+1+log(n)*n+log(n)+1+log(n)*n
         }
     }
 
     @Override
     public ArrayList<Integer> sort(ArrayList<Integer> a) {
-        bitonicSort(a, 0, a.size(), 1);
+        bitonicSort(a, 0, a.size(), 1);//1+1+log(n)+log(n)+1+n+log(n)*n+log(n)+1+log(n)*n+log(n)+1+log(n)*n=5+5*log(n)+3*log(n)*n+n=log(n)*(5+3n)+5+n
         return a;
-    }
-
-    public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(3, 7, 4, 8, 6, 2, 1, 5));
-        BitonicSort ob = new BitonicSort();
-        System.out.println(ob.sort(a));
     }
 }
